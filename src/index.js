401edd22102519369af41da1b5c4a28120e9e721
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-04-15 21:35:42 +1000

    Create form creation function.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-function component() {
+import './style.css';
+
+function Title(text) {
     const title = document.createElement('h1');
-    title.textContent = "Hello, World!";
+    title.textContent = text;
     return (
         title
     )
 }
 
-document.querySelector('body').append(component());
+function Form() {
+    const form = document.createElement('form');
+    
+    const locationLabel = document.createElement('label');
+    locationLabel.textContent = 'Location:';
+    locationLabel.setAttribute('for', 'location');
+    form.appendChild(locationLabel);
+    
+    const locationInput = document.createElement('input');
+    locationInput.id = 'location';
+    form.appendChild(locationInput);
+    
+    return (
+        form
+    )
+}
+
+document.querySelector('body').appendChild(Title('Query Weather Data!'));
+
+document.querySelector('body').appendChild(Form());
+
+
