d38ba3d3a2f2542f9e656a3924b99f21c96d62ea
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-04-16 01:18:43 +1000

    Add function to query London weather data from open weather using API key.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ function Title(text) {
     )
 }
 
+async function getLocationWeather()
+{
+    const locationInput = document.getElementById('location');
+    const response = await fetch("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=6660b08f87107700a8149f2aa3a03c67", {mode:'cors'});
+    const data = await response.json();
+    console.log(data);
+}
+
 function Form() {
     const form = document.createElement('form');
     
@@ -19,6 +27,15 @@ function Form() {
     const locationInput = document.createElement('input');
     locationInput.id = 'location';
     form.appendChild(locationInput);
+
+    const submit = document.createElement('button');
+    submit.textContent = 'get';
+    submit.addEventListener('click', getLocationWeather);
+    form.appendChild(submit);
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+    })
     
     return (
         form
